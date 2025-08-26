b0301caf789f6f015438fa2a348d782bc8187fc5
Blaž Stojanovič <blaz@kumo.ai>
2025-08-26 09:12:43 +0200

    Add `visualize_graph` tool (#23)
    Add visualize graph tool to output mermaid diagram
    
    <img width="837" height="787" alt="image"
    src="https://github.com/user-attachments/assets/0f529146-5928-471d-b5e9-0b95ce77e1ab"
    />
    
    ---------
    
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>

--- src/kumo_rfm_mcp/tools/graph_tools.py
+++ src/kumo_rfm_mcp/tools/graph_tools.py
@@ -1,4 +1,5 @@
 import logging
+from collections import defaultdict
 from typing import Any
 
 from fastmcp import FastMCP
@@ -64,10 +65,10 @@ def register_graph_tools(mcp: FastMCP):
         model to work properly. In particular,
 
         * primary keys and time columns need to be correctly specified for each
-          table in case they exist
+          table in case they exist;
         * columns need to point to a valid semantic type that describe their
-          semantic meaning, or ``None`` if they have been discarded
-        * links need to point to valid foreign key-primary key relationships
+          semantic meaning, or ``None`` if they have been discarded;
+        * links need to point to valid foreign key-primary key relationships.
 
         Returns:
             The graph metadata.
@@ -83,10 +84,10 @@ def register_graph_tools(mcp: FastMCP):
         In particular,
 
         * primary keys and time columns need to be correctly specified for each
-          table in case they exist
+          table in case they exist;
         * columns need to point to a valid semantic type that describe their
-          semantic meaning, or ``None`` if they should be discarded
-        * links need to point to valid foreign key-primary key relationships
+          semantic meaning, or ``None`` if they should be discarded;
+        * links need to point to valid foreign key-primary key relationships.
 
         Omitted fields will be untouched.
 
@@ -99,6 +100,7 @@ def register_graph_tools(mcp: FastMCP):
         Raises:
             ToolError: If table or column names do not exist.
             ToolError: If semantic types are invalid for a column's data type.
+            ToolError: If specified links are invalid.
         """
         # Only keep specified keys:
         update_dict = update.model_dump(exclude_unset=True)
@@ -156,6 +158,48 @@ def register_graph_tools(mcp: FastMCP):
         raise NotImplementedError("Link inference is not yet implemented")
 
     @mcp.tool(tags=['graph'])
-    async def visualize_graph() -> dict[str, Any]:
-        """Visualizes the graph as a mermaid diagram."""
-        raise NotImplementedError
+    async def show_graph(show_columns: bool = True) -> str:
+        """Visualize the graph as a mermaid entity relationship diagram.
+
+        Args:
+            show_columns: Whether tho show all columns in a table. If
+                ``False``, will only show the primary key, foreign key(s), and
+                time column of each table.
+        """
+        session = SessionManager.get_default_session()
+
+        fkey_dict = defaultdict(list)
+        for edge in session.graph.edges:
+            fkey_dict[edge.src_table].append(edge.fkey)
+
+        lines = ["erDiagram"]
+
+        for table in session.graph.tables.values():
+            feat_columns = []
+            for column in table.columns:
+                if (column.name != table._primary_key
+                        and column.name not in fkey_dict[table.name]
+                        and column.name != table._time_column):
+                    feat_columns.append(column)
+
+            lines.append(f"{' ' * 4}{table.name} {{")
+            if pkey := table.primary_key:
+                lines.append(f"{' ' * 8}{pkey.stype} {pkey.name} PK")
+            for fkey_name in fkey_dict[table.name]:
+                fkey = table[fkey_name]
+                lines.append(f"{' ' * 8}{fkey.stype} {fkey.name} FK")
+            if time_col := table.time_column:
+                lines.append(f"{' ' * 8}{time_col.stype} {time_col.name}")
+            if show_columns:
+                for col in feat_columns:
+                    lines.append(f"{' ' * 8}{col.stype} {col.name}")
+            lines.append(f"{' ' * 4}}}")
+
+        if len(session.graph.edges) > 0:
+            lines.append("")
+
+        for edge in session.graph.edges:
+            lines.append(f"{' ' * 4}{edge.dst_table} o|--o{{ {edge.src_table} "
+                         f": {edge.fkey}")
+
+        return '\n'.join(lines)
