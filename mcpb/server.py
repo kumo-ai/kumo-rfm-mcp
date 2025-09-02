22b12e87551f92164cec1aed4f09f75a2d119aef
rusty1s <matthias.fey@tu-dortmund.de>
2025-09-02 22:45:43 +0200

    update

--- mcpb/server.py
+++ mcpb/server.py
@@ -5,41 +5,52 @@ import sys
 from pathlib import Path
 
 ROOT = Path(__file__).parent
-VENV = ROOT / 'venv'
+SYS_VENV = ROOT / 'sys_venv'
+UV_VENV = ROOT / 'uv_venv'
 REQUIREMENTS = ROOT / 'requirements.txt'
 
 
 def get_venv() -> Path:
-    """Create a virtual environment for KumoRFM MCP via `uv`."""
-    venv_exists = VENV.exists()
-
+    """Create two virtual environments for KumoRFM MCP.
+    1. A venv that inherits system Python version and installs `uv`.
+    2. A `uv` venv with Python 3.11.
+    """
     if os.name == 'nt':
-        python = VENV / 'Scripts' / 'python.exe'
+        sys_python = SYS_VENV / 'Scripts' / 'python.exe'
+        uv_python = UV_VENV / 'Scripts' / 'python.exe'
     else:
-        python = VENV / 'bin' / 'python'
+        sys_python = SYS_VENV / 'bin' / 'python'
+        uv_python = UV_VENV / 'bin' / 'python'
 
-    if not venv_exists:
+    if not SYS_VENV.exists():
         subprocess.check_call(
-            [sys.executable, '-m', 'pip', 'install', 'uv'],
+            [sys.executable, '-m', 'venv', SYS_VENV],
+            stdout=subprocess.DEVNULL,
+        )
+
+    if not UV_VENV.exists():
+        subprocess.check_call(
+            [str(sys_python), '-m', 'pip', 'install', 'uv'],
             stdout=subprocess.DEVNULL,
         )
         subprocess.check_call(
             [
-                sys.executable, '-m', 'uv', 'venv', '--python', '3.11',
-                str(VENV)
+                str(sys_python), '-m', 'uv', 'venv', '--python', '3.11',
+                str(UV_VENV)
             ],
             stdout=subprocess.DEVNULL,
         )
 
     subprocess.check_call(
         [
-            sys.executable, '-m', 'uv', 'pip', 'install', '-r',
-            str(REQUIREMENTS), '--python', python
+            str(sys_python), '-m', 'uv', 'pip', 'install', '-r',
+            str(REQUIREMENTS), '--python',
+            str(uv_python)
         ],
         stdout=subprocess.DEVNULL,
     )
 
-    return python
+    return uv_python
 
 
 if __name__ == '__main__':
